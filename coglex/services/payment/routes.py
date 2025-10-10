a2704a9d40eadef1c70c87b4548b457820d4f0ff
IMAD EL AKHAL <imadelakhaldev@gmail.com>
2025-10-10 18:16:46 +0100

    refactor(payment): update checkout route endpoints to include 'checkout' path
    The route paths were updated to be more explicit about their purpose by adding '/checkout' to the payment service endpoints.

--- coglex/services/payment/routes.py
+++ coglex/services/payment/routes.py
@@ -18,8 +18,8 @@ from coglex.services.payment.utils import _checkout
 _payment = Blueprint("_payment", __name__)
 
 
-@_payment.route("/service/payment/v1/", methods=["POST"])
-@_payment.route("/service/payment/v1", methods=["POST"])
+@_payment.route("/service/payment/v1/checkout/", methods=["POST"])
+@_payment.route("/service/payment/v1/checkout", methods=["POST"])
 @protected()
 def checkout():
     """
